07e83c8205b4e3b99efb2746b9cbd2b8b42890a9
Valette Evan <113213514+ValetteEvan@users.noreply.github.com>
2025-12-10 12:02:40 +0100

    initial commit

---
+++ got-portfolio/src/app/shared/models/characters.model.ts
@@ -0,0 +1,11 @@
+export interface CharactersModel{
+  id: number;
+  firstName: string;
+  lastName: string;
+  fullName?: string;
+  title: string;
+  family?: string;
+  image: string;
+  imageUrl: string;
+}
+
